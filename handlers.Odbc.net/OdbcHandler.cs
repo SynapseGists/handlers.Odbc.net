93fb204998d38d38ddd53c7a50a37c5fefaf4645
steveshortt <steve.shortt@gmail.com>
2017-05-01 09:50:37 -0500

    Initial commit

---
+++ handlers.Odbc.net/OdbcHandler.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using Newtonsoft.Json;
+
+using Synapse.Core;
+
+
+public class OdbcHandler : HandlerRuntimeBase
+{
+    ConnectionInfo _dsn = null;
+
+    public override IHandlerRuntime Initialize(string config)
+    {
+        //deserialize the Config from the Handler declaration
+        _dsn = DeserializeOrNew<ConnectionInfo>( config );
+        //update for any dynamic values
+        _dsn.ConnectionString = ProcessExpressions( _dsn.ConnectionString, _dsn.Expressions );
+        return this;
+    }
+
+    public override ExecuteResult Execute(HandlerStartInfo startInfo)
+    {
+        //declare/initialize method-scope variables
+        int cheapSequence = 0; //used to order message flowing out from the Handler
+        const string __context = "Execute";
+        ExecuteResult result = new ExecuteResult()
+        {
+            Status = StatusType.Complete,
+            Sequence = Int32.MaxValue
+        };
+        string msg = "Complete";
+        Exception exc = null;
+
+        //deserialize the Parameters from the Action declaration
+        OdbcHandlerParameters parms = DeserializeOrNew<OdbcHandlerParameters>( startInfo.Parameters );
+        //update for any dynamic values
+        parms.QueryString = ProcessExpressions( parms.QueryString, parms.Expressions );
+
+        using( OdbcConnection connection = new OdbcConnection( _dsn.ConnectionString ) )
+        {
+            try
+            {
+                //if IsDryRun == true, test if ConnectionString is valid and works.
+                if( startInfo.IsDryRun )
+                {
+                    OnProgress( __context, "Attempting connection", sequence: cheapSequence++ );
+                    connection.Open();
+
+                    result.ExitData = connection.State;
+                    result.Message = msg =
+                        $"Connection test successful! Connection string: {_dsn.ConnectionString}";
+                }
+                //else, select data as declared in Parameters.QueryString
+                else
+                {
+                    //stores the data, once selected
+                    DataSet dataSet = new DataSet();
+
+                    OdbcDataAdapter adapter = new OdbcDataAdapter( parms.QueryString, connection );
+
+                    //get the data
+                    OnProgress( __context, $"Executing query: {parms.QueryString}", sequence: cheapSequence++ );
+                    connection.Open();
+                    adapter.Fill( dataSet );
+
+                    //serialize the data per requested format (Json/Xml)
+                    OnProgress( __context, "Serializing result", sequence: cheapSequence++ );
+                    string data = null;
+                    if( parms.ReturnFormat == SerializationFormat.Json )
+                        data = JsonConvert.SerializeObject( dataSet, Formatting.None );
+                    else
+                        using( StringWriter writer = new StringWriter() )
+                        {
+                            dataSet.Tables[0].WriteXml( writer );
+                            data = writer.ToString();
+                        }
+
+                    //populate the Handler result
+                    result.ExitData = data;
+                }
+            }
+            //something wnet wrong: hand-back the Exception and mark the execution as Failed
+            catch( Exception ex )
+            {
+                exc = ex;
+                result.Status = StatusType.Failed;
+                result.ExitData = msg =
+                    ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            //final runtime notification, return sequence=Int32.MaxValue by convention to supercede any other status message
+            OnProgress( __context, msg, result.Status, sequence: Int32.MaxValue, ex: exc );
+
+            return result;
+        }
+    }
+
+    string ProcessExpressions(string input, List<ExpressionItem> expressions)
+    {
+        foreach( ExpressionItem expr in expressions )
+            input = Regex.Replace( input, expr.Find, expr.ReplaceWith, RegexOptions.IgnoreCase );
+
+        return input;
+    }
+}
+
+public class ConnectionInfo
+{
+    public string ConnectionString { get; set; }
+    public List<ExpressionItem> Expressions { get; set; }
+}
+
+public enum SerializationFormat
+{
+    Json,
+    Xml
+}
+
+public class OdbcHandlerParameters
+{
+    public string QueryString { get; set; }
+    public List<ExpressionItem> Expressions { get; set; } = new List<ExpressionItem>();
+    public SerializationFormat ReturnFormat { get; set; }
+}
+
+public class ExpressionItem
+{
+    public string Find { get; set; }
+    public string ReplaceWith { get; set; }
+    public string Encoding { get; set; }
+}
---
+++ handlers.Odbc.net/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "handlers.Odbc.net" )]
+[assembly: AssemblyDescription( "Example code for custom Handler" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Synapse.Core" )]
+[assembly: AssemblyCopyright( "Copyright ©  Steve Shortt, 2016 - 2017" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "e32a1a91-0374-4fa8-8a90-e1b261e52b74" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
